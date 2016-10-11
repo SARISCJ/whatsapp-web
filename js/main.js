1a622b41e2fa2a289ac5a54a5c901e1025204b02
SARISCJ <saracastillo0@gmail.com>
2016-10-11 08:58:19 -0500

    subiendo eventos de enviar texto con hora, cambiar foto

--- js/main.js
+++ js/main.js
@@ -1,44 +1,58 @@
 ;(function() {
 
   window.addEventListener("load", cargar);
-    var contador = 1;
+
+  var input = document.getElementById("mensajes");
+  var chat = document.getElementsByClassName("avatar");
+  var conversacion = document.getElementById("conversacion");
+  var foto = document.getElementById("image");
+  var usuario = document.getElementById("user");
+  var hora = document.getElementById("hora");
 
   function cargar() {
-    var input = document.getElementById("mensajes");
-    input.addEventListener("e.keyCode==13", agregarMensaje);
+    
+    input.addEventListener("keyup", agregarMensaje);
+    for(var i = 0, longitud = chat.length; i < longitud; i++) {
+      chat[i].addEventListener("click", cambiarContac);
+    }
   }
 
-  function agregarMensaje(evento) {
-    evento.preventDefault();
+  function agregarMensaje(e) {
+    var texto = input.value.trim();
+    if(e.keyCode == 13) {
+        mostrarMensaje();
+        input.value = "";
+    }
+  }
 
-var comentario = document.getElementById("conversacion");
-    
+  function mostrarMensaje(){
     var contenedorComentario = document.createElement("div");
-    contenedorComentario.classList.add("w-message");
-
-    var contenedorPadre = document.createElement("div");
-    contenedorPadre.classList.add("w-message-text");
-
-    var usuario = document.createElement("h5");
+    contenedorComentario.classList.add("w-message", "w-message-out");
+    
+    var contenedorTexto = document.createElement("div");
+    contenedorTexto.classList.add("w-message-text");
 
     var texto = document.createElement("p");
-    texto.textContent = texto.value;
+    texto.textContent = input.value;
 
     var time = document.createElement("div");
-
-    horaPublicacion(time);
-    contenedorComentario.appendChild(usuario);
-    contenedorComentario.appendChild(texto);
-    contenedorComentario.appendChild(time);
-    comentario.appendChild(contenedorComentario);
-
-    texto.value = "";
-    contador++;
+    time.classList.add("time");
+    var fecha = new Date();
+    var hora = fecha.getHours();
+    var minutos = fecha.getMinutes();
+    var tiempo = hora + ":" + minutos;
+    time.textContent = tiempo;
+    hora.textContent = tiempo;
+      
+    contenedorTexto.insertBefore(texto,contenedorTexto.childNodes[0]);
+    contenedorTexto.insertBefore(time,contenedorTexto.childNodes[1]);
+    contenedorComentario.insertBefore(contenedorTexto,contenedorComentario.childNodes[0]);
+    conversacion.appendChild(contenedorComentario);
   }
 
-}
-
-});
+  function cambiarContac(){
+    foto.src = this.childNodes[1].src ;
+    usuario.textContent = this.childNodes[3].textContent;
+  }
 
 })();
-
